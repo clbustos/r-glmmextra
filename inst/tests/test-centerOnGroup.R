3adbd920b59d87434406087ac6c1d1ad4dfeee58
Claudio Bustos <clbustos@gmail.com>
2013-09-24 18:56:48 -0300

    Added centerOnGroup

---
+++ inst/tests/test-centerOnGroup.R
@@ -0,0 +1,8 @@
+d.f<-data.frame(g=gl(3,3),x.1=rnorm(9))
+x.means<-aggregate(data.frame(x.1.mean=d.f$x.1),list(g=d.f$g),mean)
+d.f.2<-merge(d.f,x.means)
+centered<-d.f$x.1-d.f.2$x.1.mean
+cof<-centerOnGroup(data.frame(x=d.f$x.1),d.f$g)
+expect_equal(cof$mean.x, expected=d.f.2$x.1.mean)
+expect_equal(cof$centered.x, expected=centered)
+
